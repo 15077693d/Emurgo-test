a776717011486b234a540fd114c0b365d775d6bd
Yiu Lap Sang <yiulapsang@Yius-MacBook-Air.local>
2021-11-08 10:39:20 +0800

    create Ingredient and Recipe

--- data-model/src/index.ts
+++ data-model/src/index.ts
@@ -1,1 +1,16 @@
-console.log("hello-world");
+interface Ingredient {
+  name: string;
+  calorie: number;
+}
+interface Recipe {
+  name: string;
+  foodtypes: string[];
+  allergens: string[];
+  ingredient: Ingredient[];
+}
+
+class Menu {
+  recipes: Recipe[];
+  selectedRecipe: Recipe;
+  s;
+}
